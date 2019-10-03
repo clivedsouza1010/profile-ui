e677005e4d593322b548ef06436c12a831eb3c05
clive <clive.dsouza1010@gmail.com>
2019-10-03 17:07:25 -0500

    changed directory structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import Main from './js/main';
-import { Redirect, Route, BrowserRouter } from 'react-router-dom';
+import App from './components/app';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 render((
-  <BrowserRouter>
-    <Route path="/" component={Main}/>
-  </BrowserRouter>
+  <Router>
+    <App />
+  </Router>
 ), document.getElementById('main'));
